aa785af431d472f97c206fe05df4f690594ce9fc
isaacmabueno <isaacmabueno@gmail.com>
2019-04-28 22:38:34 -0500

    axios get request test is successful

---
+++ ideaboard/src/components/IdeasContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class IdeasContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ideas: []
+        }
+    }
+// this is where I'll get the ideas from the API
+    componentDidMount() {
+        axios.get('http://localhost:3001/api/v1/ideas.json')
+        .then(response => {
+            console.log(response)
+            this.setState({ideas: response.data})
+        })
+        .catch(error=> console.log(error))
+    }
+  render() {
+    return (
+      <div>
+        {this.state.ideas.map((idea) => {
+            return(
+                <div className="tile" key={idea.id}>
+                    <h4>{idea.title}</h4>
+                    <p>{idea.body}</p>
+                </div>
+            )
+        })}
+      </div>
+    );
+  }
+}
+
+export default IdeasContainer;
