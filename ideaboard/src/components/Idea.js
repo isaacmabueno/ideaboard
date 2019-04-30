565b62bcd2aa32a422afe72bd58b7ce8370e59a3
isaacmabueno <isaacmabueno@gmail.com>
2019-04-30 16:49:47 -0500

    updating button functionality with onclick which fires off new form component

--- ideaboard/src/components/Idea.js
+++ ideaboard/src/components/Idea.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Idea = ({idea}) => 
-<div className='tile'>
-<h4>{idea.title}</h4>
-<p>{idea.body}</p>
-</div>
+    <div className='tile'>
+        <h4>{idea.title}</h4>
+        <p>{idea.body}</p>
+    </div>
 
 export default Idea;
--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import Idea from './Idea'
+import IdeaForm from './IdeaForm'
+import update from 'immutability-helper'
 
 class IdeasContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            ideas: []
+            ideas: [],
+            editingIdeaId: null,
+
         }
     }
 // this is where I'll get the ideas from the API
     componentDidMount() {
         axios.get('http://localhost:3001/api/v1/ideas.json')
         .then(response => {
-            console.log(response)
             this.setState({ideas: response.data})
         })
         .catch(error=> console.log(error))
     }
+
+    addNewIdea = () => {
+        axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: "", body: ""}})
+        .then(response => {
+            console.log(response)
+            const ideas = update(this.state.ideas, { $splice: [[0, 0, response.data]]})
+            this.setState({ideas: ideas, editingIdeaId: response.data.id})
+        })
+        .catch(error=> console.log(error))
+    }
+
   render() {
     return (
       <div>
         <div>
-            <button className='newIdeaButton'>New Idea</button>
+            <button className='newIdeaButton' onClick={this.addNewIdea}>New Idea</button>
         </div>
         {this.state.ideas.map((idea) => {
-        return(
-            <Idea idea={idea} key={idea.id}/>
-        )
+            if(this.state.editingIdeaId === idea.id) {
+                return(<IdeaForm idea={idea} key={idea.id} />)
+            } else {
+                return(<Idea idea={idea} key={idea.id}/>)
+            }
+        
         })}
       </div>
     );
