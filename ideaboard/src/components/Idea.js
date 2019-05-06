ad1d90cdc31768b5f6687a607c79f9b4085316c5
isaacmabueno <isaacmabueno@gmail.com>
2019-05-05 20:22:17 -0500

    creating delete capability and styling

--- ideaboard/src/components/Idea.js
+++ ideaboard/src/components/Idea.js
@@ -3,9 +3,14 @@ class Idea extends Component {
     handleClick = () => {
         this.props.onClick(this.props.idea.id)
     }
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.idea.id)
+    }
     render() {
         return(
             <div className='tile'>
+            <span className='deleteIdea' onClick={this.handleDelete}>X</span>
                 <h4 onClick={this.handleClick}>{this.props.idea.title}</h4>
                 <p onClick={this.handleClick}>{this.props.idea.body}</p>
             </div>
--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -25,7 +25,6 @@ class IdeasContainer extends Component {
     addNewIdea = () => {
         axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: "", body: ""}})
         .then(response => {
-            console.log(response)
             const ideas = update(this.state.ideas, { $splice: [[0, 0, response.data]]})
             this.setState({ideas: ideas, editingIdeaId: response.data.id})
         })
@@ -37,6 +36,17 @@ class IdeasContainer extends Component {
         const ideas = update(this.state.ideas,{[ideaIndex]: { $set: idea}})
         this.setState({ideas: ideas, notification: 'Changes saved!'})
     }
+
+    deleteIdea = (id) => {
+        axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+        .then(response => {
+            const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+            const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+            this.setState({ideas: ideas})
+        })
+        .catch(error=> console.log(error))
+    }
+
     resetNotification = () => {this.setState({notification: ''})}
 
     enableEditing = (id) => {
@@ -61,6 +71,7 @@ class IdeasContainer extends Component {
             } else {
                 return(<Idea idea={idea} key={idea.id}
                     onClick={this.enableEditing}
+                    onDelete={this.deleteIdea}
                     />)
             }
         
